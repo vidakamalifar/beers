d55d0b82b1a95405f1b113b15f6404fb76d95d1a
vida kamalifar <kamalifar.vida@gmail.com>
2021-01-02 16:29:05 +0200

    BaseDialog class

---
+++ app/src/main/java/com/fob/beers/dialog/BaseDialog.kt
@@ -0,0 +1,44 @@
+package com.fob.beers.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.View
+import android.view.Window
+
+abstract class BaseDialog(context: Context) {
+
+    internal lateinit var parentView: View
+    private var dialog: Dialog
+
+    init {
+        dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setCanceledOnTouchOutside(true)
+    }
+
+    fun setParentView(p: View) {
+        parentView = p
+    }
+
+    fun show() {
+        dialog.setContentView(parentView)
+        dialog.show()
+    }
+
+    fun dismiss() {
+        dialog.dismiss()
+    }
+
+    fun isShowing(): Boolean {
+        return dialog.isShowing
+    }
+
+    fun getDialog(): Dialog? {
+        return dialog
+    }
+
+
+}
