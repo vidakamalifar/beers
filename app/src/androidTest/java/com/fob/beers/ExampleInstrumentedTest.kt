f092944b726b0d3054c438f505194f1abaf19196
vida kamalifar <kamalifar.vida@gmail.com>
2021-01-03 21:04:02 +0200

    Add Test for Db

--- app/src/androidTest/java/com/fob/beers/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fob/beers/ExampleInstrumentedTest.kt
@@ -1,12 +1,22 @@
 package com.fob.beers
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import com.fob.beers.dao.BeerDAO
+import com.fob.beers.databease.AppDatabase
+import com.fob.beers.model.Beer
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.IOException
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +30,41 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.fob.beers", appContext.packageName)
+    }
+
+
+    @RunWith(AndroidJUnit4::class)
+    class SimpleEntityReadWriteTest {
+        private lateinit var mUserDao: BeerDAO
+        private lateinit var mDb: AppDatabase
+
+        @Before
+        fun createDb() {
+            val context = ApplicationProvider.getApplicationContext<Context>()
+            mDb = Room.inMemoryDatabaseBuilder(
+                context, AppDatabase::class.java
+            ).build()
+            mUserDao = mDb.beerDAO!!
+
+        }
+
+        @After
+        @Throws(IOException::class)
+        fun closeDb() {
+            mDb.close()
+        }
+
+        @Test
+        @Throws(Exception::class)
+        fun writeUserAndReadInList() {
+            val beerList: ArrayList<Beer?> = ArrayList()
+            for (i in 0..3) {
+                val beer = Beer("vida$i")
+                beerList.add(beer)
+            }
+            mUserDao.insert(beerList)
+            val byName = mUserDao.getBeers()
+            assertThat("Db Test Result", byName?.get(0)?.name, equalTo("vida0"))
+        }
     }
 }
